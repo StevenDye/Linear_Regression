a8b1237fc9d8b8bff34f713405e08a09a87aed16
Steven Dye <steven.p.dye@gmail.com>
2019-07-20 17:00:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 style.use('fivethirtyeight')
 
 #Creates random data
-def create_dataset(how_many, variance, step=2, correlation=False):
+def create_dataset(how_many, variance, step=0, correlation=False):
 	val = 1
 	ys = []
 	for i in range(how_many):
@@ -15,7 +15,7 @@ def create_dataset(how_many, variance, step=2, correlation=False):
 		ys.append(y)
 		if correlation == 'pos':
 			val+=step
-		elif correlation == 'neg':
+		if correlation == 'neg':
 			val-=step
 	xs = [i for i in range(len(ys))]
 
@@ -38,7 +38,18 @@ def coefficient_of_determination(ys_orig, ys_line):
 	squared_error_y_mean = squared_error(ys_orig, y_mean_line)
 	return 1 - (squared_error_regr / squared_error_y_mean)
 
-xs, ys = create_dataset(40, 40, 2, correlation='pos') #Creates data with pos correlation
+data_amount = int(input('How many data points do you want to create? '))
+noise = int(input('How much randomness do you want in the data? (insert a positive number): '))
+print('If you want the data to be positively correlated, type "pos".')
+print('If you want the data to be negatively correlated, type "neg".')
+print('If you do not want the data correlated, type anything else. ')
+cor = input()
+
+if cor == 'pos' or cor == 'neg':
+	steepness = int(input('How steep do you want the slope to be?: '))
+else: steepness = 0
+
+xs, ys = create_dataset(data_amount, noise, steepness, correlation=cor) #Creates data with pos correlation
 
 #Perform calculations
 m,b = best_fit_slope_and_intercept(xs,ys)
@@ -46,12 +57,14 @@ regression_line = [(m*x) + b for x in xs]
 r_squared = coefficient_of_determination(ys, regression_line)
 
 #Makes a prediction
-predict_x = 8
+predict_x = int(input('What value would you like to make a prediction at?: '))
 predict_y = (m * predict_x) + b
 
 #Output
-print(r_squared)
-print(m,b)
+print('The r squared value is: {}'.format(r_squared))
+print('The slope is: {}'.format(m))
+print('The y intercept is: {}'.format(b))
+print('The y value of the prediction value is {}'.format(predict_y))
 
 plt.scatter(xs,ys)
 plt.scatter(predict_x,predict_y, s=100, color='g')
